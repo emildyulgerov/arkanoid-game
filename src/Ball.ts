ce013d15403cd0575b5e028fbfde84dc7865e331
Emil Dyulgerov <emil.dyulgerov@dyulgerov-len>
2022-12-14 22:41:04 +0200

    side fix

--- src/Ball.ts
+++ src/Ball.ts
@@ -1,9 +1,12 @@
 import { height, width, x, y } from ".";
+import { Brick } from "./Brick";
+import { Paddle } from "./Paddle";
 import { Vector } from "./Point";
 
 export class Ball {
     public radius = 10;
     public circle = new Vector(500, 500);
+    public alive = true;
     constructor(public context: CanvasRenderingContext2D,
         public vel: Vector,
         public x: number,
@@ -20,8 +23,8 @@ export class Ball {
     moveBall(){
         this.circle.add(this.vel)
 
-        if (this.circle.y >= height - 10){
-            this.vel.y = -this.vel.y;
+        if (this.circle.y > height - 10){
+            this.alive = false;
         }
         if (this.circle.x >= width - 10){
             this.vel.x = -this.vel.x
@@ -33,5 +36,33 @@ export class Ball {
             this.vel.x = -this.vel.x
         }
     }
+    paddleCheck(paddle: Paddle){
+        if(this.circle.y + this.vel.y >= paddle.y - this.radius){
+            if(this.circle.x + this.vel.x >= paddle.x && this.circle.x + this.vel.x <= paddle.x + paddle.width){
+                const paddleCenter = paddle.x + paddle.width / 2;
+                const distFromCenter = this.circle.x - paddleCenter;
+                this.vel.x = distFromCenter * 0.05
+                this.vel.y = -this.vel.y
+            }
+        }
+    }
+
+    brickCheck(bricks: Brick[]){
+        for (let b of bricks){
+            if(this.sideBrickHit(b)){
+                this.vel.x = -this.vel.x;
+            }
+            if (this.circle.x > b.x && this.circle.x < b.x + b.width
+                && this.circle.y + this.radius > b.y
+                && this.circle.y - this.radius < b.y + b.height){
+                    b.active = false;
+                    this.vel.y = 3 * 1.5
+                }
+        }
+    }
+    sideBrickHit(brick: Brick): boolean{
+        return this.circle.y > brick.y + this.vel.y && this.circle.y < brick.y + brick.height + this.vel.y;
+    }
+    
         
 }
