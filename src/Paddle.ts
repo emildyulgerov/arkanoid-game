e5d6fab4d526d2c855994c2946f1a45128760251
Emil Dyulgerov <emil.dyulgerov@dyulgerov-len>
2022-12-14 22:41:35 +0200

    Paddle class ready

--- src/Paddle.ts
+++ src/Paddle.ts
@@ -1,3 +1,4 @@
+import { height, width } from ".";
 import { Point } from "./Point";
 
 export class Paddle {
@@ -13,37 +14,24 @@ export class Paddle {
         public color?: string
         ){
         this.x = x;
-        this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
     }
 
-    drawPaddle(): void {
+    drawPaddle(x: number): void {
         if (this.color) {
             this.context.fillStyle = this.color;
         }
         this.context.beginPath();
-        this.context.moveTo(this.x, this.y);
-        this.context.rect(this.x, this.y, this.width, this.height);
+        if (x >= width - this.width){
+            x = width - this.width;
+        } else if (x <= 0){
+            x = 0 
+        }
+        this.context.rect(x, this.y, this.width, this.height);
         this.context.fill();
         this.context.stroke();
         this.context.closePath();
-    }
-
-    movePaddle(): void {  
-        document.addEventListener('keydown', event => {
-        
-            if (event.code == 'ArrowRight') {
-                if (this.x <= 1000 - this.width) {
-                    this.x += this.speed.x
-                }
-
-            } else if (event.code == 'ArrowLeft') {
-                if (this.x >= 0) {
-                    this.x -= this.speed.x
-                }
-            }
-        })
     }
 }
