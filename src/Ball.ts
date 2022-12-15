e5874f1e16365eecf4b2bc674345294dab34d34c
Emil Dyulgerov <emil.dyulgerov@dyulgerov-len>
2022-12-15 11:41:27 +0200

    fix

--- src/Ball.ts
+++ src/Ball.ts
@@ -27,41 +27,44 @@ export class Ball {
             this.alive = false;
         }
         if (this.circle.x >= width - 10){
-            this.vel.x = -this.vel.x
+            this.vel.x = -this.vel.x * 1.2
         }
         if (this.circle.y <= 10){
-            this.vel.y = -this.vel.y;
+            this.vel.y = -this.vel.y * 1.2;
         }
         if (this.circle.x <= 10){
-            this.vel.x = -this.vel.x
+            this.vel.x = -this.vel.x * 1.2
         }
     }
     paddleCheck(paddle: Paddle){
-        if(this.circle.y + this.vel.y >= paddle.y - this.radius){
+        if(this.circle.y + this.vel.y >= paddle.y - this.vel.y){
             if(this.circle.x + this.vel.x >= paddle.x && this.circle.x + this.vel.x <= paddle.x + paddle.width){
                 const paddleCenter = paddle.x + paddle.width / 2;
                 const distFromCenter = this.circle.x - paddleCenter;
                 this.vel.x = distFromCenter * 0.05
-                this.vel.y = -this.vel.y
+                this.vel.y = -this.vel.y * 1.0001
             }
         }
     }
 
     brickCheck(bricks: Brick[]){
         for (let b of bricks){
-            if(this.sideBrickHit(b)){
-                this.vel.x = -this.vel.x;
-            }
             if (this.circle.x > b.x && this.circle.x < b.x + b.width
                 && this.circle.y + this.radius > b.y
                 && this.circle.y - this.radius < b.y + b.height){
-                    b.active = false;
-                    this.vel.y = 3 * 1.5
-                }
+                    if(this.sideBrickHit(b)){
+                        this.vel.x = -this.vel.x;
+                        console.log('sidehit')
+                    } else {
+                        this.vel.y = -this.vel.y 
+                        b.active = false;
+                    }
+                } 
         }
     }
     sideBrickHit(brick: Brick): boolean{
-        return this.circle.y > brick.y + this.vel.y && this.circle.y < brick.y + brick.height + this.vel.y;
+        return this.circle.y > brick.y && this.circle.y < brick.y + brick.height && this.circle.x >= brick.x && this.circle.x <= brick.x + brick.width;
+        //return this.circle.y > brick.y + this.vel.y && this.circle.y < brick.y + brick.height + this.vel.y;
     }
     
         
